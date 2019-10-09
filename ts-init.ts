26797b84e6b2f31aeafda550a7869cb13057e192
exw <ewu120@protonmail.com>
2019-10-09 18:05:07 -0400

    Typescript project initialization

---
+++ ts-init.ts
@@ -0,0 +1,27 @@
+/* 2019-10-19
+ * tsc --version === Version 3.6.3
+ * npm -v === 6.12.0 
+ *
+ * (See https://medium.com/@vincentnewkirk/npm-vs-yarn-2019-e88757b17038 
+ * for npm vs yarn.)
+ *
+ * Initializing a typescript application. 
+ * 
+ * /
+
+Commands:
+
+npx gts init //https://github.com/google/ts-style
+npm i tslint
+npm i tslint-config-airbnb
+// npm i @types/node //?
+npm i
+
+
+/* If unable/unwilling to use gtx:
+ * npm init
+ * npm i typescript @types/node
+ * npx tsc --init
+ * npm i tslint
+ * npm i tslint-config-airbnb
+ * /
